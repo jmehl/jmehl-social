ca8dd542565e94af4744692a431fcc842dcff789
Joseph Mehler <jmmehler@gmail.com>
2022-09-19 14:16:48 -0400

    deploy setup

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,7 @@ import { LOGOUT } from '../actions/types';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
